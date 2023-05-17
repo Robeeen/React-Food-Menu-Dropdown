4701e45d684a043621fb69c214b6732ddb520e80
Shams Khan <robeeen@gmail.com>
2023-05-17 07:20:10 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+//import './App.css';
+import CartClass from './components/CartClass';
+import CartFunction from './components/CartFunction';
+import Counter from './components/Counter';
+import ListView from './components/ListView';
+import Weather from './components/Weather';
+import DropDown from './components/DropDown';
+import SelectDropdown from './components/SelectDropdown';
+import ReactReduce from './components/ReactReduce';
+import ReactReduceObject from './components/ReactReduceObject';
+//import Deck from './components/Deck';
+import Category from './components/Category';
+import FoodCategory from './components/FoodCategory';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CartFunction />
+      <CartClass />
+      <Counter />
+      <Weather />
+      <ListView /> 
+      <DropDown />*/}
+      {/* <SelectDropdown /> */}
+      {/* <ReactReduce /> */}
+      {/* <ReactReduceObject /> */}
+      {/* <Deck /> */}
+      {/* <Category /> */}
+      <FoodCategory />
+    </div>
+  );
+}
+
+
+export default App;
